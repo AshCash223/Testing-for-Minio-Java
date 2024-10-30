ad84dcd39f6fe8253685ae98bccdc1b901fb4ef7
coonamatata <riley.cooney123@gmail.com>
2024-10-30 15:29:51 +1300

    added two new permissions in the manifest to get access to media and images. Added java code to ask for permission for media and images. The implementation still requires some work however.

--- app/src/main/java/com/example/miniotester/MainActivity.java
+++ app/src/main/java/com/example/miniotester/MainActivity.java
@@ -1,31 +1,30 @@
 package com.example.miniotester;
 
+import android.Manifest;  // Ensure to import Manifest
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.InputStream;
-import java.util.ArrayList;
 
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
@@ -33,17 +32,11 @@ import io.minio.errors.MinioException;
 
 public class MainActivity extends AppCompatActivity {
 
-    //variable to hold a tag to search for in logcat
     private static final String TAG = "MinioHelloWorld";
-    //setting variable to the end point to exaba server
     private static final String ENDPOINT = "https://z4-n1.dv.exaba.io:9000";
-    //variable to hold to public access key
     private static final String ACCESS_KEY = "JC5QAFBT2OUHJRJJ";
-    //variable to hold to private access key
     private static final String SECRET_KEY = "IeQWblMFbQ3RjAjEBMCIOqwSSbYgs2PdMicxwGjR";
-    //variable to the name of the bucket selected
     private static String BUCKET_NAME = "";
-    //variable for image selection process
     private static final int PICK_IMAGE_REQUEST = 1;
 
     private Button syncButton;
@@ -51,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         syncButton = findViewById(R.id.syncButton);
@@ -67,13 +59,51 @@ public class MainActivity extends AppCompatActivity {
         });
 
         BUCKET_NAME = getIntent().getStringExtra("radioText");
+
+        // Check if the media permissions are granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED) {
+
+            // Permissions not granted, request them
+            Log.w(TAG, "Permissions not granted");
+            askForPermission();
+        } else {
+            // Permissions are already granted
+            Log.w(TAG, "Permissions granted");
+        }
     }
+
+    // Method to request media permissions from the user
+    public void askForPermission() {
+        ActivityResultLauncher<String[]> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestMultiplePermissions(),
+                result -> {
+                    Boolean imagePermissionGranted = result.getOrDefault(Manifest.permission.READ_MEDIA_IMAGES, false);
+                    Boolean videoPermissionGranted = result.getOrDefault(Manifest.permission.READ_MEDIA_VIDEO, false);
+
+                    // If both permissions are granted
+                    if (imagePermissionGranted != null && imagePermissionGranted
+                            && videoPermissionGranted != null && videoPermissionGranted) {
+                        Log.w(TAG, "Permissions are now granted");
+                        Toast.makeText(MainActivity.this, "Permissions granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Permissions were not granted
+                        Log.w(TAG, "Permissions were not granted");
+                        Toast.makeText(MainActivity.this, "Permissions are required to upload images", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        activityResultLauncher.launch(new String[]{
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.READ_MEDIA_VIDEO
+        });
+    }
+
     public void onClickSettings(View view) {
         Intent intent = new Intent(MainActivity.this, Settings.class);
-        //starting the activity with the intent
         startActivity(intent);
     }
-    //method that runs the file manager
+
     private void openFilePicker() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.setType("image/*");
@@ -81,23 +111,21 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
-    //This method checks if a file has been selected in the file manager, if an image has been selected it then calls the image upload method
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getClipData() != null) {
-
-            ClipData filess = data.getClipData();
-            for(int x = 0; x< filess.getItemCount(); x++)
-            {
-                Uri imageUri = filess.getItemAt(x).getUri();
+            ClipData files = data.getClipData();
+            for (int x = 0; x < files.getItemCount(); x++) {
+                Uri imageUri = files.getItemAt(x).getUri();
                 String objectName = getFileName(imageUri);
                 new MinioUploadTask(imageUri, objectName).execute();
             }
         }
     }
-    //this method gets the file name that was uploaded
+
     private String getFileName(Uri uri) {
         String file = "File";
         try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
@@ -112,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onclickDisplay(View view) {
-        // Get the URL from the intent
         String Url = getIntent().getStringExtra("drawableURL");
         if (Url == null) {
             Log.e(TAG, "URL is null");
@@ -122,42 +149,34 @@ public class MainActivity extends AppCompatActivity {
         startActivity(swap);
     }
 
-
-    //This class uploads the chosen file to the bucket
     private class MinioUploadTask extends AsyncTask<Void, Void, Boolean> {
 
-        //declaring variables to hold important values
         private final Uri imageUri;
         private final String objectName;
 
-        //this constructor initialises the image and the object names
         public MinioUploadTask(Uri imageUri, String objectName) {
             this.imageUri = imageUri;
             this.objectName = objectName;
         }
 
-        //This method puts the file into the bucket
         @Override
         protected Boolean doInBackground(Void... voids) {
-            try (InputStream inputStream = getContentResolver().openInputStream(imageUri)) {{
+            try (InputStream inputStream = getContentResolver().openInputStream(imageUri)) {
                 if (inputStream == null) return false;
 
-                //buiding the minIO client
                 MinioClient minioClient = MinioClient.builder()
                         .endpoint(ENDPOINT)
                         .credentials(ACCESS_KEY, SECRET_KEY)
                         .build();
-                    // uploading the image
-                    minioClient.putObject(
-                            PutObjectArgs.builder()
-                                    .bucket(BUCKET_NAME)
-                                    .object(objectName)
-                                    .stream(inputStream, inputStream.available(), -1)
-                                    .contentType("image/jpeg") // Adjust content type if necessary
-                                    .build()
-                    );
-                }
 
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(BUCKET_NAME)
+                                .object(objectName)
+                                .stream(inputStream, inputStream.available(), -1)
+                                .contentType("image/jpeg") // Adjust content type if necessary
+                                .build()
+                );
 
                 Log.d(TAG, "Image uploaded successfully: " + objectName);
                 return true;
@@ -170,7 +189,6 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
 
-        //this method displays a toast to the user to tell them whether the upload was successful or not
         @Override
         protected void onPostExecute(Boolean success) {
             if (success) {
